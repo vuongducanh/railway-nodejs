46184d013ab90b87cec3a68fdff2f8ac4e0137c3
anhvd001 <anhvd@nal.vn>
2024-09-30 21:22:12 +0700

    add socket io 2

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const path = require('path');
 const indexRouter = require('./routes/index');
-const http = require("http");
 const app = express();
+
+const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: "*", // Cho phép tất cả các domain
-    methods: ["GET", "POST"],
-    credentials: false // Nếu bạn cần hỗ trợ cookie
-  }
-});
+const io = new Server(server);
 
 const PORT = 3000;
 
@@ -23,7 +18,7 @@ io.on("connection", function (socket) {
     "user-joined",
     socket.id,
     io.engine.clientsCount,
-    Object.keys(io.sockets.clients().sockets)
+    Object.keys(io.sockets.sockets)
   );
 
   socket.on("signal", (toId, message) => {
@@ -48,6 +43,6 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
   });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
